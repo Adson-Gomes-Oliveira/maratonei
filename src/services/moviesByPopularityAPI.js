b9b0b2011f5e3cd22c944f127532e1979aeeff24
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-04-18 17:08:55 -0300

    :wrench: <fix>: Fixing remote repository 19

--- src/services/moviesByPopularityAPI.js
+++ src/services/moviesByPopularityAPI.js
@@ -6,7 +6,7 @@ export async function moviesByPopularity() {
         .then((response) => response.json())
         .then((response) => response.results);
 
-    const secondRequestPopularity = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
+    const secondRequestPopularity = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_watch_monetization_types=flatrate`)
         .then((response) => response.json())
         .then((response) => response.results);
 
