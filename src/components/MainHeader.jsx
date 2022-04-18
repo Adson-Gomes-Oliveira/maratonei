ef9c137e4cb40715da92d9ce29a24c3fb02d260c
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-04-18 16:55:54 -0300

    :wrench: <fix>: Fixing remote repository 13

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Logo from '../images/svg/logotipo.svg';
-import LoginIcon from '../images/svg/login.svg';
-import {HeaderStyled, MenuStyled, MenuLoginStyled} from '../styles/index';
+import {HeaderStyled} from '../styles/index';
+import MenuBar from './MenuBar';
 
 function MainHeader() {
   return (
@@ -14,17 +14,7 @@ function MainHeader() {
           data-testid="maratonei-logo-svg"
         />
       </Link>
-      <MenuStyled aria-label="main-menu">
-        <Link to="/home">INICIO</Link>
-        <Link to="/tv-series">TV SERIES</Link>
-        <Link to="/films">FILMES</Link>
-        <Link to="/streamings">STREAMINGS</Link>
-        <Link to="/library">BIBLIOTECA</Link>
-        <MenuLoginStyled>
-          <Link to="/sign-in">ENTRAR</Link>
-          <img src={LoginIcon} alt="Icone de Login" />
-        </MenuLoginStyled>
-      </MenuStyled>
+      <MenuBar />
     </HeaderStyled>
   );
 }
--- src/context/MaratoneiProvider.js
+++ src/context/MaratoneiProvider.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import MaratoneiContext from './MaratoneiContext';
+import moviesByPopularity from '../services/moviesByPopularityAPI';
 
 function MaratoneiProvider({children}) {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMoviesByPopularity = async (page) => {
+    setLoading(true);
+
+    const moviesData = await moviesByPopularity(page);
+    setPopularMovies(moviesData);
+
+    setLoading(false);
+  };
+
   return (
     <MaratoneiContext.Provider value={ {
-
+      popularMovies,
+      fetchMoviesByPopularity,
+      loading,
     } }>
       {children}
     </MaratoneiContext.Provider>
