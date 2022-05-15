45d350e19fb870e44244d709aa08e558dc3d2d9a
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-05-14 22:44:19 -0300

    :sparkles: <feat>: Filter just by year is now working

--- src/services/requestByQuery.js
+++ src/services/requestByQuery.js
@@ -7,7 +7,15 @@ async function fetchMoviesByQuery(search, year) {
     return data;
   }
 
-  const requestByQuery = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${search}&language=pt-BR&year=${year}&include_adult=false&include_video=false&page=1&sort_by=popularity.desc&with_watch_monetization_types=flatrate`)
+  if (search === '') {
+    const requestByQuery = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&with_watch_monetization_types=flatrate`)
+        .then((response) => response.json())
+        .then((response) => response.results);
+
+    return requestByQuery;
+  }
+
+  const requestByQuery = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${search}&language=pt-BR&primary_release_year=${year}&include_adult=false&include_video=false&page=1&sort_by=popularity.desc&with_watch_monetization_types=flatrate`)
       .then((response) => response.json())
       .then((response) => response.results);
 
@@ -18,6 +26,14 @@ async function fetchSeriesByQuery(search, year) {
   if (search === '' && year === '') {
     const data = requestByPopularity('/series');
     return data;
+  }
+
+  if (search === '') {
+    const requestByQuery = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${TMDB_API_KEY}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&first_air_date_year=${year}&with_watch_monetization_types=flatrate`)
+        .then((response) => response.json())
+        .then((response) => response.results);
+
+    return requestByQuery;
   }
 
   const requestByQuery = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${TMDB_API_KEY}&query=${search}&language=pt-BR&first_air_date_year=${year}&timezone=America%2FSao_Paulo&sort_by=popularity.desc&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`)
