e3d2b3aa7cd33316f59852d59bf72b805eb61425
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-06-23 23:08:49 -0300

    :sparkles: <general>: Project Finished - To Infinity and Beyond

--- src/styles/menu.js
+++ src/styles/menu.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const MenuStyled = styled.nav`
+const MenuStyled = styled.nav`
   display: flex;
   font-family: 'Bebas', sans-serif;
   font-size: var(--sizing-4x);
@@ -21,23 +21,4 @@ export const MenuStyled = styled.nav`
   }
 `;
 
-export const MenuLoginStyled = styled.div`
-  display: flex;
-  justify-content: center;
-
-  a {
-    align-items: center;
-    display: flex;
-    justify-content: center;
-  }
-
-  span:nth-child(1) {
-    position: relative;
-    top: 0.05rem;
-  }
-
-  span:nth-child(2) {
-    font-size: 1.5rem;
-    margin-left: 1rem;
-  }
-`;
+export default MenuStyled;
